7d50c70660786aff5573e7eda7ad20a3b7856c32
MauroKkkk <mmcaf1@aluno.ifal.edu.br>
2023-05-18 19:09:11 -0300

    1.0.1

---
+++ src/data.tsx
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    },
+    {
+        topico: "Laboratório",
+        titulo: "Novos equipamentos",
+        descricao: "Novos equipamentos chegam ao campus",
+        data: "13/05/23"
+    }
+]
